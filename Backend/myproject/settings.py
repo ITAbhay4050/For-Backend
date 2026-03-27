36df00668e45172268e823442d4c9daec7a6065b
ITAbhay4050 <it02comptech@gmail.com>
2026-03-27 16:34:24 +0530

    Fix build and render files

--- Backend/myproject/settings.py
+++ Backend/myproject/settings.py
@@ -176,12 +176,12 @@ CSRF_TRUSTED_ORIGINS = [
 # -----------------------------------------------------------------------------
 # Email (SMTP) - use Render env vars
 # -----------------------------------------------------------------------------
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_HOST_USER = 'it02comptech@gmail.com'
+EMAIL_HOST_PASSWORD = 'ytno qhlv ihnz mqlx'
 
 # -----------------------------------------------------------------------------
 # Security (safe for production)
